d39a9a45b85c4f22754c82dc6e6cfcaa33625b8f
bhavinaa <bhavina.sk.04@gmail.com>
2025-01-22 21:57:02 +0800

    Level 2. Add, List

--- src/main/java/bhavs.java
+++ src/main/java/bhavs.java
@@ -1,28 +1,51 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class bhavs {
     public static void main(String[] args) {
+        // initialisation
         String logo = "bhavs";
-
+        List<String> list = new ArrayList<>();
+        // introduction
         System.out.println(" Hello from\n" + logo);
-        System.out.println("I repeat whatever you say, just say \"bye\" if you want to end it with me");
+        System.out.println("I help to keep track of what " +
+                "you said, just say \"list\" if you do " +
+                "not want to see the current list just " +
+                "say \"bye\" if you want to end it with me");
         System.out.println("____________________________________________________________");
         System.out.println(" What is your name?");
         Scanner sc = new Scanner(System.in);
         String answer = sc.nextLine();
         System.out.println(" Hi " + answer +"!");
         System.out.println(" You have a cool name.");
-        System.out.println(" What can I do for you?");
+        System.out.println(" What can I add in the list");
         System.out.println("____________________________________________________________");
         Scanner sc1 = new Scanner(System.in);
         String answer1 = sc1.nextLine();
-        while (!"bye".equals(answer1)) {
-            System.out.println("You said: " + answer1 + ". That's a good one!");
-            System.out.println("do you have anything else to say");
+        int count = 0;
+        list.add(answer1);
+
+        while (true) {
+            System.out.println("do you want to add anything else");
             sc1 = new Scanner(System.in);
             answer1 = sc1.nextLine();
+            if("bye".equals(answer1)) {
+                System.out.println(" Bye." + answer + "! Hope to see you again soon!");
+                break;
+            }
+            if("list".equals(answer1)) {
+                for (int i = 0; i < list.size(); i ++) {
+                    System.out.println(i + ". " +  list.get(i));
+                }
+                continue;
+            }
+            list.add(answer1);
+            System.out.println("added: " + answer1);
+
         }
 
-        System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 }
